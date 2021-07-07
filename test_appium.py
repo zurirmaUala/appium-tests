deca713775dc45dca989cbc9ba1ce77811391a5c
zurirmarodriguez <zurirma.rodriguez@uala.com.ar>
2021-07-07 16:53:57 -0300

    2 tests completed

--- test_appium.py
+++ test_appium.py
@@ -2,7 +2,9 @@ import time
 
 from appium import webdriver
 import pytest
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 desired_caps = {
     "browser": "appium",
@@ -21,18 +23,42 @@ class Testappium():
         assert True
 
     def test_login(self):
-        driver.find_element_by_xpath('hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]').click()
-        time.sleep(5)
-        driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/signInOutButton').click()
-        time.sleep(5)
-        driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/signin_username_input').send_keys("new_user_1606759264")
-        time.sleep(5)
-        driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/signin_password_input').send_keys("Test1234")
-        time.sleep(5)
-        driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/sign_in_button').click()
-        time.sleep(5)
-        arg_text = driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/environmentText').text
-        assert arg_text == 'Argentina (STAGE)'
+        wait = WebDriverWait(driver, 20)
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.XPATH,'hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]')))
+        # driver.find_element_by_xpath('hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]').click()
+        currently_waiting_for.click()
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID,'ar.com.bancar.uala.cryptoapp.stage:id/signInOutButton')))
+        currently_waiting_for.click()
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID,'ar.com.bancar.uala.cryptoapp.stage:id/signin_username_input')))
+        currently_waiting_for.send_keys("new_user_1606759264")
+        # driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/signin_username_input').send_keys("new_user_1606759264")
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID,'ar.com.bancar.uala.cryptoapp.stage:id/signin_password_input')))
+        currently_waiting_for.send_keys("Test1234")
+        # driver.find_element_by_id('ar.com.bancar.uala.cryptoapp.stage:id/signin_password_input').send_keys("Test1234")
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID,'ar.com.bancar.uala.cryptoapp.stage:id/sign_in_button')))
+        currently_waiting_for.click()
+        arg_text = wait.until(EC.text_to_be_present_in_element((By.ID, 'ar.com.bancar.uala.cryptoapp.stage:id/environmentText'),'Argentina (STAGE)'))
+        # arg_text.text
+        assert arg_text == True
+
+    def test_purchase(self):
+        wait = WebDriverWait(driver, 90)
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID,'ar.com.bancar.uala.cryptoapp.stage:id/go_to_exchange_button')))
+        currently_waiting_for.click()
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.XPATH,'/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.TextView')))
+        currently_waiting_for.click()
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.XPATH,'/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.view.ViewGroup')))
+        currently_waiting_for.click()
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.XPATH, '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.EditText')))
+        currently_waiting_for.send_keys('100,00')
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID, 'ar.com.bancar.uala.cryptoapp.stage:id/confirmationButton')))
+        currently_waiting_for.click()
+        currently_waiting_for = wait.until(EC.element_to_be_clickable((By.ID, 'ar.com.bancar.uala.cryptoapp.stage:id/confirmationButton')))
+        currently_waiting_for.click()
+        successful_text = wait.until(EC.text_to_be_present_in_element((By.ID,'ar.com.bancar.uala.cryptoapp.stage:id/feedback_title'),'¡Se generó la compra!'))
+        assert successful_text == True
+
+
 
 
 
